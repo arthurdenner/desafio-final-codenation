2601ebb100aaaac8059f625ab7ca83205c096c7c
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-25 20:42:58 -0300

    Adiciona o component input

---
+++ src/components/Input/Input.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const InputDefault = styled.input`
+  width: 400px;
+  height: 25px;
+  padding: 10 0px;
+  font-size: 1.2em;
+  margin: 0 auto;
+  border-radius: 0.3em;
+`;
+
+const Input = props => (
+  <InputDefault
+    name={props.name}
+    component='input'
+    placeholder={props.placeholder}
+  />
+);
+
+export default Input;
