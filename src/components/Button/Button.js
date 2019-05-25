53c98fbc4e3002e053b1169b3fddefad59c674c5
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-25 12:30:15 -0300

    Adiciona o button.

---
+++ src/components/Button/Button.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const ButtonDefault = styled.button`
+  color: tomato;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid wheat;
+  border-radius: 3px;
+  width: 85px;
+  height: 25px;
+`;
+
+const Button = props => <ButtonDefault>{props.text}</ButtonDefault>;
+
+export default Button;
