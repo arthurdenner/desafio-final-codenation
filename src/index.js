56596a6dc191407f4225b406e9642ae0cdbc8d7c
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-27 17:23:23 -0300

    Adiciona o arquivo rota.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import Routers from "../src/router/Routers";
 import { Provider } from "react-redux";
-import { createStore, compose } from "redux";
-import rootReducers from "./redux/rootReducers";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducers from "../src/redux/rootReducers";
+import createSagaMiddleware from "redux-saga"; // SAGA
+import rootSagas from "../src/redux/rootSagas";
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-let store = createStore(rootReducers, {}, composeEnhancers());
+let store = createStore(
+  rootReducers,
+  {},
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+
+// run the saga
+sagaMiddleware.run(rootSagas);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Routers />
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
