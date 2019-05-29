d12b0c2c40d44d328406ca169f81995937bef40f
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-05-29 19:54:58 -0300

    Altera a action  searchRepos.

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,84 +1,84 @@
-import Api from '../utils/api'
+import Api from "../utils/api";
 import {
-    REQUEST_USERDATA,
-    RECEIVE_USERDATA,
-    RECEIVE_USERDATA_ERROR,
-    REQUEST_USERREPOS,
-    RECEIVE_USERREPOS,
-    RECEIVE_USERREPOS_ERROR,
-} from './constants';
+  REQUEST_USERDATA,
+  RECEIVE_USERDATA,
+  RECEIVE_USERDATA_ERROR,
+  REQUEST_USERREPOS,
+  RECEIVE_USERREPOS,
+  RECEIVE_USERREPOS_ERROR
+} from "./constants";
 
 export function requestUserData() {
-    return {
-        type: REQUEST_USERDATA,
-    };
+  return {
+    type: REQUEST_USERDATA
+  };
 }
 
 function receiveUserData(json) {
-    return {
-        type: RECEIVE_USERDATA,
-        userData: json,
-    };
+  return {
+    type: RECEIVE_USERDATA,
+    userData: json
+  };
 }
 
 function receiveUserDataErr(error) {
-    return {
-        type: RECEIVE_USERDATA_ERROR,
-        error,
-    };
+  return {
+    type: RECEIVE_USERDATA_ERROR,
+    error
+  };
 }
 
 function requestRepos() {
-    return {
-        type: REQUEST_USERREPOS,
-    };
+  return {
+    type: REQUEST_USERREPOS
+  };
 }
 
 function receiveRepos(json) {
-    return {
-        type: RECEIVE_USERREPOS,
-        repos: json,
-    };
+  return {
+    type: RECEIVE_USERREPOS,
+    repos: json
+  };
 }
 
 function receiveReposErr(error) {
-    return {
-        type: RECEIVE_USERREPOS_ERROR,
-        error,
-    };
+  return {
+    type: RECEIVE_USERREPOS_ERROR,
+    error
+  };
 }
 
 export function fetchUserData(user) {
-    return dispatch => {
-        dispatch(requestUserData());
-        return Api.get(`/users/${user}`)
-            .then(json => dispatch(receiveUserData(json)))
-            .catch(err => dispatch(receiveUserDataErr(err)));
-    };
+  return dispatch => {
+    dispatch(requestUserData());
+    return Api.get(`/users/${user}`)
+      .then(json => dispatch(receiveUserData(json)))
+      .catch(err => dispatch(receiveUserDataErr(err)));
+  };
 }
 
 function fetchRepos(user) {
-    return dispatch => {
-        dispatch(requestRepos());
-        return Api.get(`/users/${user}/repos`)
-            .then(json => dispatch(receiveRepos(json)))
-            .catch(err => dispatch(receiveReposErr(err)));
-    };
+  return dispatch => {
+    dispatch(requestRepos());
+    return Api.get(`/users/${user}/repos`)
+      .then(json => dispatch(receiveRepos(json)))
+      .catch(err => dispatch(receiveReposErr(err)));
+  };
 }
 
 export function fetchUserAndRepos(user) {
-    return (dispatch, getState) => {
-        return dispatch(fetchUserData(user)).then(() => {
-            return dispatch(fetchRepos(user));
-        });
-    };
+  return (dispatch, getState) => {
+    return dispatch(fetchUserData(user)).then(() => {
+      return dispatch(fetchRepos(user));
+    });
+  };
 }
 
-function searchRepos(language) {
-    return dispatch => {
-        dispatch(requestRepos());
-        return Api.get(`/search/repositories?q=language%3A${language}`)
-            .then(json => dispatch(receiveRepos(json)))
-            .catch(err => dispatch(receiveReposErr(err)));
-    };
+export function searchRepos(search) {
+  return dispatch => {
+    dispatch(requestRepos());
+    return Api.get(`/search/repositories?q=${search}`)
+      .then(json => dispatch(receiveRepos(json.data)))
+      .catch(err => dispatch(receiveReposErr(err)));
+  };
 }
